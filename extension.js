4d1d4facd76dbb5af5d1856dca6ff3bd3f9b8574
Ricardo Huamani <ricardohuamanip@gmail.com>
2019-12-20 15:08:29 -0500

    feat: move notifications to the right

--- extension.js
+++ extension.js
@@ -1,63 +1,14 @@
-const St = imports.gi.St;
+const Clutter = imports.gi.Clutter;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 
-let text, button;
-
-function _hideHello() {
-  Main.uiGroup.remove_actor(text);
-  text = null;
-}
-
-function _showHello() {
-  let monitor = Main.layoutManager.primaryMonitor;
-
-  if (!text) {
-    text = new St.Label({
-      style_class: 'helloworld-label',
-      text: "Hello, world!",
-    });
-    Main.uiGroup.add_actor(text);
-  }
-
-  text.opacity = 255;
-
-  text.set_position(
-    monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-    monitor.y + Math.floor(monitor.height / 2 - text.height / 2),
-  );
-
-  Tweener.addTween(text, {
-    opacity: 0,
-    time: 2,
-    transition: 'easeOutQuad',
-    onComplete: _hideHello,
-  });
-}
-
-function init() {
-  button = new St.Bin({
-    style_class: 'panel-button',
-    reactive: true,
-    can_focus: true,
-    x_fill: true,
-    y_fill: false,
-    track_hover: true,
-  });
-
-  let icon = new St.Icon({
-    icon_name: 'system-run-symbolic',
-    style_class: 'system-status-icon',
-  });
-
-  button.set_child(icon);
-  button.connect('button-press-event', _showHello);
-}
+function init() {}
 
 function enable() {
-  Main.panel._rightBox.insert_child_at_index(button, 0);
+  Main.messageTray._bannerBin.set_x_align(Clutter.ActorAlign.END);
+  Main.messageTray._bannerBin.set_y_align(Clutter.ActorAlign.START);
 }
 
 function disable() {
-  Main.panel._rightBox.remove_child(button);
+  Main.messageTray._bannerBin.set_x_align(Clutter.ActorAlign.CENTER);
+  Main.messageTray._bannerBin.set_y_align(Clutter.ActorAlign.START);
 }
